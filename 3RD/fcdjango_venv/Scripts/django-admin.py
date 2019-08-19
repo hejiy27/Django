30e3a965da8b330453bdd8fa2c359c564d0908b3
hejiy27 <hejiy27@gmail.com>
2019-08-19 20:34:08 +0900

    update

---
+++ 3RD/fcdjango_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\seokjun\desktop\fastcampus\3rd\fcfjango_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
